ef2f9fab088fe6a7a432515fad002b0fe2200b50
Sarabdeep <noorraikhi18@gmail.com>
2024-08-11 23:37:52 +0530

    part3

--- mernapp/src/App.js
+++ mernapp/src/App.js
@@ -1,13 +1,31 @@
 
 import './App.css';
 import Home from './screens/Home';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Login from './screens/Login';
+import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 
 function App() {
   return (
-    <>
-     <div><Home/></div>
-    </>
+
+    <Router>
+     <div>
+
+    <Routes>
+
+     <Route exact path="/" element = {<Home/>} /> 
+     <Route exact path="/login" element = {<Login/>} /> 
+
+    </Routes>
+
+     </div>
+    </Router>
    
   );
 }
