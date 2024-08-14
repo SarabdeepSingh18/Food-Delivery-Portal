db0b9276db34763f477833721811741814371b35
Sarabdeep <noorraikhi18@gmail.com>
2024-08-14 14:46:59 +0530

    Part-5

--- mernapp/backend/db.js
+++ mernapp/backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://swigato:18032003@cluster0.mmhot.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const mongoURI = 'mongodb+srv://swigato:18032003@cluster0.mmhot.mongodb.net/Swigato?retryWrites=true&w=majority&appName=Cluster0';
 
 const mongoDB = async () => {
   try {
@@ -8,6 +8,12 @@ const mongoDB = async () => {
       useUnifiedTopology: true,
     });
     console.log('MongoDB connected successfully');
+    
+    const fetched_data = mongoose.connection.db.collection("fooditems");
+
+    const data = await fetched_data.find({}).toArray(); // Await the data retrieval
+    console.log(data);  // Log the data after it's fully retrieved
+
   } catch (err) {
     console.error('MongoDB connection error:', err);
     process.exit(1); 
